cf5270240736d4878184a24531e56c973359451e
Pascal Brandt <psbrandt@gmail.com>
2019-10-23 11:39:06 -0700

    🏓 Add CSV viewer.

--- viewer/ui/filleviewer.tsx
+++ viewer/ui/filleviewer.tsx
@@ -9,6 +9,7 @@ import {
   Classes,
   NonIdealState
 } from "@blueprintjs/core";
+import CsvToJson from "csvtojson";
 
 const API_ROOT = "http://localhost:3000";
 
@@ -37,11 +38,64 @@ const TextViewer = props => {
   );
 };
 
+const CSVViewer = props => {
+  const [csv, setCsv] = useState([]);
+
+  useEffect(() => {
+    fetch(props.url)
+      .then(res => res.text())
+      .then(text => {
+        CsvToJson()
+          .fromString(text)
+          .then(data => setCsv(data));
+      });
+  }, [props.url]);
+
+  const renderTableHeader = cols => {
+    return (
+      <thead>
+        <tr>
+          {cols.map((column, i) => (
+            <th key={`${i}${column}`}>{column}</th>
+          ))}
+        </tr>
+      </thead>
+    );
+  };
+
+  const renderTableBody = (keys, rows) => {
+    return (
+      <tbody>
+        {rows.map((row, i) => (
+          <tr key={i}>
+            {keys.map((column, i) => (
+              <td key={`${i}${row[column]}`}>{row[column]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    );
+  };
+
+  if (!csv.length) return null;
+
+  return (
+    <div className="pkb__fileviewer__csv__wrapper">
+      <table className="pkb__fileviewer__csv__table bp3-html-table bp3-html-table-striped bp3-interactive">
+        {renderTableHeader(Object.keys(csv[0]))}
+        {renderTableBody(Object.keys(csv[0]), csv)}
+      </table>
+    </div>
+  );
+};
+
 const renderFileViewer = fileObject => {
   if (fileObject.type === "application/pdf") {
     return <PDFViewer url={getUrl(fileObject.url, fileObject.phenotype.id)} />;
   } else if (fileObject.type === "text/plain") {
     return <TextViewer url={getUrl(fileObject.url, fileObject.phenotype.id)} />;
+  } else if (fileObject.type === "text/csv") {
+    return <CSVViewer url={getUrl(fileObject.url, fileObject.phenotype.id)} />;
   } else {
     return (
       <NonIdealState
