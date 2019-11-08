4180e17742dc8a9530cf0fabcf43593223dd7c53
Pascal Brandt <psbrandt@gmail.com>
2019-11-08 13:58:20 -0800

    🍡 Fix Docker repo URL.

--- deployment/docker-deploy.sh
+++ deployment/docker-deploy.sh
@@ -6,7 +6,7 @@ echo "$BINTRAY_API_KEY" | docker login -u "$BINTRAY_USERNAME" --password-stdin p
 set -o xtrace
 
 # Push the image
-docker push phema-docker-docker.bintray.io/phema-phex:$TRAVIS_TAG
+docker push phema-docker-docker.bintray.io/phema-phekb-export-viewer:$TRAVIS_TAG
 
 # Notify to slack
 SLACK_MESSAGE="New PheKB Export Viewer <https://bintray.com/beta/#/phema/docker/phema-phekb-export-viewer?tab=overview|Docker image> published ($TRAVIS_TAG)"
